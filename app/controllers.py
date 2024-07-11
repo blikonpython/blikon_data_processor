083679bca6157fca7540ac1b385aecaa8fbc6728
msedeno23 <msedeno.blikon@gmail.com>
2024-07-11 10:45:22 -0600

    Update data paths in controllers.py
    The specific data paths in various methods of the FirebaseModel controller have been simplified. The prefix "blikon/" was removed from all paths, leaving only the exact data fields as the arguments for the get_data() method. This should enhance the readability and maintainability of the code.

--- app/controllers.py
+++ app/controllers.py
@@ -5,16 +5,16 @@ class DataController:
         self.model = FirebaseModel()
 
     def get_conversions(self):
-        return self.model.get_data("blikon/conversionCelMXaPersonal")
+        return self.model.get_data('conversionCelMXaPersonal')
 
     def get_conversion_porcentaje_activacion(self):
-        return self.model.get_data("blikon/conversionPorcentajeActivacion")
+        return self.model.get_data('conversionPorcentajeActivacion')
 
     def get_growth(self):
-        return self.model.get_data("blikon/crecimientoAcumuladoUsuarios")
+        return self.model.get_data('crecimientoAcumuladoUsuarios')
 
     def get_user_breakdown(self):
-        return self.model.get_data("blikon/desgloseUsuarios")
+        return self.model.get_data('desgloseUsuarios')
 
     def get_popular_places(self):
-        return self.model.get_data("blikon/placesMasUsados")
+        return self.model.get_data('placesMasUsados')
